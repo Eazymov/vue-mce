3158d1ae25a961c6e7efea295a3bf0689073dfd2
Eduard <eazymov@devvela.com>
2017-11-07 16:26:42 +0500

    added jasmine-spec-reporter package

--- test/unit/runner.js
+++ test/unit/runner.js
@@ -1,6 +1,12 @@
 import Jasmine from 'jasmine';
+import { SpecReporter } from 'jasmine-spec-reporter';
 
 const jasmine = new Jasmine();
+const reporter = new SpecReporter({
+  showPending: true,
+});
 
+jasmine.env.clearReporters();
+jasmine.env.addReporter(reporter);
 jasmine.loadConfigFile('test/unit/jasmine.json');
 jasmine.execute();
